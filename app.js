6885c193c3639a236b3dc0edb25d27832667849b
puchk <kpuchalski@gmail.com>
2017-12-05 13:40:16 -0800

    creates gallery post route, adds bodyparser

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 8080;
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
@@ -17,8 +18,12 @@ app.get('/gallery', function(req, res) {
 		{name: 'The Scream', image: 'http://wisetoast.com/wp-content/uploads/2015/10/the-scream-edvard-munch-poster.jpg'},
 		{name: 'American Gothic', image: 'http://wisetoast.com/wp-content/uploads/2015/10/American-Gothic-grant-wood-painting.jpg'}
 	];
-	
+
 	res.render('gallery', {paintings:paintings});
+});
+
+app.post('/gallery', function(req, res) {
+	res.send("Post route");
 })
 
 app.listen(port, function(err) {
