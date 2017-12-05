3ab0160db97cc09c6d7953fad068c38b13214ca3
puchk <kpuchalski@gmail.com>
2017-12-05 13:02:34 -0800

    initial files, routing, and displaying content

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.get('/gallery', function(req, res) {
+	var paintings = [
+		{name: 'Mona Lisa', image: 'http://wisetoast.com/wp-content/uploads/2015/10/mona-lisa-leonardo-da-vinci-most-famous-painting.jpg'},
+		{name: 'Starry Night', image: 'http://wisetoast.com/wp-content/uploads/2015/10/Starry-Night-by-Vincent-Van-Gogh-painting.jpg'},
+		{name: 'The Scream', image: 'http://wisetoast.com/wp-content/uploads/2015/10/the-scream-edvard-munch-poster.jpg'},
+		{name: 'American Gothic', image: 'http://wisetoast.com/wp-content/uploads/2015/10/American-Gothic-grant-wood-painting.jpg'}
+	];
+	
+	res.render('gallery', {paintings:paintings});
+})
+
+app.listen(port, function(err) {
+	console.log('Listening on port ' + port);
+});
