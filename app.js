4cb584ac99ad82b3ed153e2a77dd47f9b3bd81be
puchk <kpuchalski@gmail.com>
2017-12-05 17:10:52 -0800

    adds mongoose, displays paintings, ability to add paintings, data persistence

--- app.js
+++ app.js
@@ -1,33 +1,63 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var app = express();
 var port = process.env.PORT || 8080;
 
+mongoose.connect('mongodb://localhost/peluce');
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-var paintings = [
-	{name: 'Mona Lisa', image: 'http://wisetoast.com/wp-content/uploads/2015/10/mona-lisa-leonardo-da-vinci-most-famous-painting.jpg'},
-	{name: 'Starry Night', image: 'http://wisetoast.com/wp-content/uploads/2015/10/Starry-Night-by-Vincent-Van-Gogh-painting.jpg'},
-	{name: 'The Scream', image: 'http://wisetoast.com/wp-content/uploads/2015/10/the-scream-edvard-munch-poster.jpg'},
-	{name: 'American Gothic', image: 'http://wisetoast.com/wp-content/uploads/2015/10/American-Gothic-grant-wood-painting.jpg'}
-];
+// SCHEMA
+var paintingSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+var Painting = mongoose.model("Painting", paintingSchema);
+
+/*Painting.create(
+		{
+			name: 'Starry Night', 
+			image: 'http://wisetoast.com/wp-content/uploads/2015/10/Starry-Night-by-Vincent-Van-Gogh-painting.jpg'		
+		}, function(err, painting){
+			if(err){
+				console.log(err);
+			} else {
+				console.log("New Painting Added");
+				console.log(painting);
+			}
+		}
+)*/
+
 
 app.get('/', function(req, res) {
 	res.render('index');
 });
 
 app.get('/gallery', function(req, res) {
-	res.render('gallery', {paintings:paintings});
+	Painting.find({},function(err, paintings){
+		if(err){
+			console.log(err);
+		} else {
+			res.render('gallery', {paintings:paintings});
+		}
+	});
 });
 
 app.post('/gallery', function(req, res) {
 	var name = req.body.name;
 	var image = req.body.image;
 	var newPainting = {name: name, image: image};
-	paintings.push(newPainting);
-	res.redirect("/gallery");
+	Painting.create(newPainting, function(err, addedPainting){
+		if(err) {
+			console.log(err);
+		} else {
+				res.redirect("/gallery");
+		}
+	})
 })
 
 app.get('/gallery/new', function(req, res) {
